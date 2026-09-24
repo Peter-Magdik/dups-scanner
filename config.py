9c050569df398ef8c6f1684199ef2efdf1f91b23
Peter Magdík <petermagdik@gmail.com>
2026-09-24 22:33:20 +0200

    fix: corrected spelling error

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Config:
     source: str
     target: str
-    hashing_algo: str
+    hashing_algorithm: str
     hashing_mode: str
     delete: bool
     quiet: bool
