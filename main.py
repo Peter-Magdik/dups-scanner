b080cd3c5097b4fec556eebc7cf0cdf4aade7108
Peter Magdík <petermagdik@gmail.com>
2026-09-25 19:54:30 +0200

    refactor:  add catch of all unexpacted errors

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
+import logging
+import sys
+
 import cli
+import constants
 from config import Config
 from controller import Controller
+
+logger: logging.Logger = logging.getLogger(name=__name__)
 
 
 def main() -> None:
     config: Config = cli.cli_to_config()
 
-
     app: Controller = Controller(config)
     app.run()
 
+
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception:
+        logger.exception(msg="Unexpected error")
+        sys.exit(constants.EXIT_UNEXPECTED)
